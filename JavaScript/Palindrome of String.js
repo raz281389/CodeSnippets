3518a2b2c0549ab58b502338525d7c1767139311
RAJESH KUMAR <raz281389@users.noreply.github.com>
2016-07-06 21:44:49 -0700

    Update Palindrome of String.js

--- JavaScript/Palindrome of String.js
+++ JavaScript/Palindrome of String.js
@@ -3,7 +3,7 @@ function palindrome(str) {
   str=str.toLowerCase();
   //str=str.replace(/\w+/g ,'');
   str=str.replace(/\s+/g ,'');
- str=str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g ,'');
+ str=str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g ,''); //puntuation order
   var strTest=str.split('').reverse().join('');
   strTest=strTest;
   strTest=strTest;
