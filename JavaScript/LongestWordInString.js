6d4bfe0483b4c005f61c84c6a29a5d06a3f429fc
RAJESH KUMAR <raz281389@users.noreply.github.com>
2016-07-06 22:00:14 -0700

    Create LongestWordInString.js

---
+++ JavaScript/LongestWordInString.js
@@ -0,0 +1,15 @@
+
+function longestWord(str) {
+var array = ["a"];
+
+  var string = str.split(' ');
+  for (i = 0;i < string.length; i++) {
+        if (string[i].length > array[0].length) {
+            array.splice(0,1);
+            array.push(string[i]);
+        }
+    }
+    return array[0].length;
+}
+
+longestWord("What if we try a super-long word such as otorhinolaryngology");
