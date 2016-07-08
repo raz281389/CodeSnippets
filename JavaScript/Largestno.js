474a483e1db8c83bbef91dcedd8f8c2bcbcf6b28
RAJESH KUMAR <raz281389@users.noreply.github.com>
2016-07-07 21:49:27 -0700

    Create Largestno.js
    Largest No from each array

---
+++ JavaScript/Largestno.js
@@ -0,0 +1,19 @@
+function largestOfFour(arr) {
+  var array =[""];
+for (var i= 0; i < arr.length; i++)
+    {
+      var check = arr[i];
+       var xy =0;
+      for (var j = 0; j< check.length; j++)
+        {
+          if (xy < check[j])
+            {
+              xy = check[j];
+            }
+             array [i]= xy;
+        }
+    }
+  // You can do this!
+  return array;
+}
+largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
