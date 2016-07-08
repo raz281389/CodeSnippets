5a464c2088f547aa66adf5883a0358a0bd91017d
RAJESH KUMAR <raz281389@users.noreply.github.com>
2016-07-07 21:50:25 -0700

    Update Largestno.js

--- JavaScript/Largestno.js
+++ JavaScript/Largestno.js
@@ -13,7 +13,6 @@ for (var i= 0; i < arr.length; i++)
              array [i]= xy;
         }
     }
-  // You can do this!
   return array;
 }
 largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
